204e4b25ae2636833962a6a261336626b0233ca8
psrinivas2 <psrinivas2@cordys.com>
2011-08-09 08:21:38 +0000

    Fix for Issue 14 messageSelector support for Durable subscribers

--- src/java/com/cordys/coe/ac/jmsconnector/Trigger.java
+++ src/java/com/cordys/coe/ac/jmsconnector/Trigger.java
@@ -858,8 +858,8 @@ public class Trigger
 				}
 				if (m_destination.getInnerDestination() instanceof Topic)
 				{
-					m_consumer = session.createDurableSubscriber((Topic)m_destination.getInnerDestination(),this.m_subscriptionName,
-															this.messageSelector,false);
+					m_consumer = session.createDurableSubscriber((Topic)m_destination.getInnerDestination(),m_subscriptionName,
+															messageSelector,false);
 				}
 				else
 				{
